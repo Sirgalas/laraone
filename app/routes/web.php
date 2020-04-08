ff9bf2d34cc07ee737328c8a32e9a49c9d672ac8
Sirgalas <hudos@rambler.ru>
2020-04-09 00:12:06 +0300

    add route

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about',function (){
+    return 'About';
+})->name('about');
+
+Route::get('/posts/{id}',function ($id){
+    return 'Posts: '.$id;
+})->where('id','[0-9]+');
+
+Route::get('/posts/{id}/comments/{comment?}',function ($id, $comment=101){
+    return 'Posts: '.$id. " Commnets ".$comment;
+})->where(['id'=>'[0-9]+','comment'=>'[0-9]+']);
+
+Route::post('/posts',function (){
+    return 'Posts';
+});
+
+Route::match(['get','post'],'/posts-Two',function (){
+    return 'Post Two';
+});
+
+Route::group(['prefix'=>'/post', 'as'=>'post.'],function (){
+    Route::get('/',function (){
+        return "
+        <div>Posts</div>
+        <a href='".route('about')."'>About</a>
+        <a href='".route('post.show',['id'=>1])."'>Post first</a>
+    ";
+    })->name('index');
+    Route::get('/{id}',function ($id){
+        return 'Posts: '.$id;
+    })->name('show');
+});
+
